5af9b948e5c7c09a57c5e5dc8804cf736166cd9d
Rene Horn <the.rhorn@gmail.com>
2011-04-15 02:07:14 -0500

    Refactored parser module to rollparse to end conflict.

--- dispatchdebug.py
+++ dispatchdebug.py
@@ -1,14 +1,14 @@
-import parser
+import rollparse
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 production = "roll"
 
-rollparser = parser.RollParser( parser.declaration )
+rollparser = rollparse.RollParser( rollparse.declaration )
 
-rollparser.parse("[d6+d8+5]+3", processor=parser.dispatcher.RollDispatcher() )
+rollparser.parse("[d6+d8+5]+3", processor=rollparse.dispatcher.RollDispatcher() )
 
-rollparser.parse("{2xd6}+3+3xd8", processor=parser.dispatcher.RollDispatcher() )
+rollparser.parse("{2xd6}+3+3xd8", processor=rollparse.dispatcher.RollDispatcher() )
 
-rollparser.parse("{2xd6]+3+3xd8", processor=parser.dispatcher.RollDispatcher() ) #should give error
+rollparser.parse("{2xd6]+3+3xd8", processor=rollparse.dispatcher.RollDispatcher() ) #should give error
