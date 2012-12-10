4983c5bfefac3d0bb19b928044c4b92535d6572c
Rene Horn <the.rhorn@gmail.com>
2012-12-10 00:42:37 -0600

    Modified for PyInstaller 2.0.

--- wincompile.sh
+++ wincompile.sh
@@ -3,8 +3,9 @@
 PYDIR="c:/Python27"
 PYTHON="wine $PYDIR/python.exe"
 
-cd pyinstaller
+#cd pyinstaller
 
-$PYTHON Configure.py
-$PYTHON Makespec.py ../diceroller.py
-$PYTHON Build.py diceroller/diceroller.spec
+#$PYTHON Configure.py
+#$PYTHON Makespec.py ../diceroller.py
+#$PYTHON Build.py diceroller/diceroller.spec
+$PYTHON pyinstaller/pyinstaller.py diceroller.py
