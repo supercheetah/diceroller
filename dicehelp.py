ca1c5dc19c6103223da147ed4f7835027af4d315
Rene Horn <the.rhorn@gmail.com>
2013-01-04 01:24:42 -0600

    Added some help about the history.

--- dicehelp.py
+++ dicehelp.py
@@ -7,6 +7,8 @@ _______________________""", "2d4 + 3d6 + 5"),
 _______________________""", "5d8 * (d4 + 10)"),
 ("""I can take an arbitrary number of sides, so a d9 is perfectly valid.
 
+    You'll also notice that I record a history of all your rolls on the right.  Just tape them to repeat the roll, or use the up and down arrows to go through the history.
+
     Expandable rolls: 3x2d9 will become 2d9 + 2d9 +2d9, which is, yes, the same as 6d6, but it's just another way of writing it that I will will accept.
 
     More useful for that: separated rolling if you need to, say, roll a d20 three times, but separately, and add 15 to them.
