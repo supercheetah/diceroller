d45922d569e69882cb2ef503f68106b835593886
Rene Horn <the.rhorn@gmail.com>
2013-01-15 01:18:26 -0600

    Added d4-loading.png to the list of files to be copied for the apk.

--- apk_files.sh
+++ apk_files.sh
@@ -2,4 +2,4 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 rm android_essentials/*.pyo
-cp code.py COPYING d100.png d10.png d12.png d20.png d4.png d6.png d8.png dice_compiler.py dicehelp.py dice.kv diceroller.spec dispatchdebug.py dispexcept.py enum.py eqnstrings.py icon-from-convertico.com.ico icon-from-gimp.ico icon.ico icon.png __init__.py lexer.py main.py parsertest.py README.rst rollenum.py rollil.py rollit.spec rollparse.py varstore.py version android_essentials
+cp code.py COPYING d100.png d10.png d12.png d20.png d4.png d6.png d8.png d4-loading.png dice_compiler.py dicehelp.py dice.kv diceroller.spec dispatchdebug.py dispexcept.py enum.py eqnstrings.py icon-from-convertico.com.ico icon-from-gimp.ico icon.ico icon.png __init__.py lexer.py main.py parsertest.py README.rst rollenum.py rollil.py rollit.spec rollparse.py varstore.py version android_essentials
